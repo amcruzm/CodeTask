76308352e7c5d66ce646c0c360c55705dd7c31e4
Andrea Cruz Moreno <a.cruz@appdev.de>
2023-07-04 10:43:52 +0200

    Add controller

--- src/main/java/com/example/anagramcheckerapplication/AnagramCheckerApplication.java
+++ src/main/java/com/example/anagramcheckerapplication/AnagramCheckerApplication.java
@@ -2,12 +2,28 @@ package com.example.anagramcheckerapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AnagramCheckerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnagramCheckerApplication.class, args);
+    }
+}
+@RestController
+class AnagramController {
+
+    @GetMapping("/anagrams")
+    public ResponseEntity<Boolean> checkAnagrams(
+            @RequestParam String text1,
+            @RequestParam String text2
+    ) {
+        boolean areAnagrams = AnagramChecker.areAnagrams(text1, text2);
+        return ResponseEntity.ok(areAnagrams);
     }
 }
 
